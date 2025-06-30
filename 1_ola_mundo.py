f99b96f5e4b068e69bcb804764521a05c5282270
Luan-Felipe-R-Sanches <luanfeliperodrigues25@gmail.com>
2025-06-30 12:33:38 -0400

    first commit

---
+++ 1_ola_mundo.py
@@ -0,0 +1,9 @@
+def main():
+    # Exibindo "Olá, Mundo!" no console
+    print("Olá, Mundo!")
+    # Modificando para exibir o nome
+    print("Meu nome é Luan.")
+
+
+if __name__ == "__main__":
+    main()

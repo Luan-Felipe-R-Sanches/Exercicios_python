86c32784084ce5a205d86dfeb10244d1d1a65e3d
Luan-Felipe-R-Sanches <luanfeliperodrigues25@gmail.com>
2025-07-16 17:58:05 -0400

    Adicionando Exercicios

--- 13_estrutura_repeticao.py
+++ 13_estrutura_repeticao.py
@@ -12,3 +12,23 @@ for i in'Nikola Tesla':
 lista10 = ['Água', 'Açúcar', 'Arroz', 'Pão', 'Leite', 'Carne', 'Refrigerante']
 for i in lista10:
     print(i)
+
+"""
+Crie um programa que lê um valor de início e um valor de fim, 
+exibindo em tela a contagem dos números dentro desse intervalo.
+incio = int(input('Digite o número onde começa a contagem: '))
+fim = int(input('Digite o número onde termina a contagem: '))
+
+for i in range(incio, fim+1):
+    print(i)
+"""
+    
+"""
+Crie um programa que realiza a contagem de 0 a 20, exibindo apenas os números pares:
+"""
+for i in range(0, 21):
+    if i % 2 == 0:
+        print(i)
+        
+for i in range(0,21,2):
+    print(i)
---
+++ 14_tabuada.py
@@ -0,0 +1,4 @@
+x = int(input('Digite um Número: '))
+
+for num in range(1,11):
+    print(f'{x} X {num} = {x * num}')
